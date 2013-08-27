e7408245ccd5227809d79e014c95750801b38ded
Matt Rubin <mattrubin@me.com>
2013-08-27 02:09:49 -0400

    Let's call it Paper Chain

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('readingApp', [])
+angular.module('paperchain', [])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('readingApp')
+angular.module('paperchain')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
